07ab6c767557eeb4952a4a891b7ba136d47de6d7
Victor <Victor.Gamble@outlook.com>
2020-04-07 10:26:41 -0400

    white space changes.

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { connect } from "react-redux";
-import { eat, nap, play } from "../redux/Actions";
+import { eat, nap, play } from '../redux/Actions';
 
 
 
 const Activity = ({activity, eat, play, nap}) => (
-    <>
+    <div>
         <h1>What is the cat doing?</h1>
         <p>The cat is {activity}</p>
         <button onClick={() => eat()}>Eating</button>
         <button onClick={() => play()}>Playing</button>
         <button onClick={()=> nap()}>Napping</button>
-    </>
+    </div>
 )
 
 const mapStateToProps = state => {
--- src/redux/Actions.js
+++ src/redux/Actions.js
@@ -2,14 +2,14 @@ import { Action_Nap, Action_Eat, Action_Play } from './ActionTypes';
 
 
 export const nap = () => {
-    return Action_Nap;
+    return { type: Action_Nap };
 }
 
 export const eat = () => {
-    return Action_Eat;
+    return { type: Action_Eat };
 }
 
 export const play = () => {
-    return Action_Play;
+    return { type: Action_Play };
 }
 
